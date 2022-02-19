76185de11179c5e9d96104126209c057550c550c
Artem <merkushynletter13@gmail.com>
2022-02-19 22:55:22 +0200

    changed the mobile-menu and header

--- js/js.js
+++ js/js.js
@@ -2,5 +2,6 @@ let mobBtn = document.querySelector('.mobile-menu__btn');
 let mobMenu = document.querySelector('.mobile-menu');
 
 mobBtn.onclick = function() {
+   mobBtn.classList.toggle('active');
    mobMenu.classList.toggle('active');
 }
