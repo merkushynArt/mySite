8dace3baa128ee10e4a22c2cbd212860a2203b8b
Artem <merkushynletter13@gmail.com>
2022-03-10 12:40:28 +0200

    created a experiments-section

--- js/experiments.js
+++ js/experiments.js
@@ -31,11 +31,34 @@ function timerStart() {
    timerEl.innerHTML = `${hours}:${minutes}:${seconds}`;
 
    if(time === 0){
-      clearInterval(timeId)
+      clearInterval(timeId);
    }
-   time--
+   time--;
 }
 
 
 
-//текст из частиц
+//дождь
+function rain() {
+   let amount = 80;
+   let body = document.querySelector('.rain');
+   let i = 0;
+   while(i < amount) {
+      let drop = document.createElement('i');
+
+      let size = Math.random() * 5;
+      let posX = Math.floor(Math.random() * window.innerWidth);
+      let delay = Math.random() * -20;
+      let duration = Math.random() * 5;
+
+      drop.style.width = 0.2 + size + 'px';
+      drop.style.left = posX + 'px';
+      drop.style.animationDelay = delay + 's';
+      drop.style.animationDuration = 1 + duration + 's';
+
+      body.appendChild(drop);
+      i++;
+   }
+}
+
+rain();
