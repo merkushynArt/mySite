f4bf9053389141234dac36717de32806eef9bb07
merkushynArtem <merkushynwrite@gmail.com>
2023-04-19 00:51:06 +0300

    implemented a preloader

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
+import { useEffect } from 'react';
 import './App.css';
+import { Preloader } from './components/Preloader';
 
 function App() {
+   useEffect(() => {
+      const timer = setTimeout(() => {
+         document.body.classList.add('loaded');
+      }, 2000);
+      return () => clearTimeout(timer);
+   }, []);
+
    return (
       <div className="App">
-         
+         <Preloader/>
+         <h1>hello</h1>
       </div>
    );
 }
---
+++ client/src/components/Preloader.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Preloader = () => {
+   return (
+      <div className='preload'>
+         <p className='preload__text'>Loading</p>
+      </div>
+   );
+}
+
